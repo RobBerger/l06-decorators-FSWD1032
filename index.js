1797ab68baa18958caede802719049ae4bcb25ac
berger <robertberger.dev@gmail.com>
2023-01-26 10:01:41 -0500

    updated code example and ran npm start

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 "use strict";
-function identity(idObj) {
-    let { value1, value2 } = idObj;
-    console.log(value1 + ": " + typeof (value1));
-    console.log(value2 + ": " + typeof (value2));
-    return idObj;
+// interface Identity <T, U> {
+//     value1: T,
+//     value2: U
+// }
+// function identity<T,U>(idObj: Identity<T, U>): Identity<T,U> {
+//     let {value1, value2} = idObj;
+//     console.log(value1 +": " + typeof(value1));
+//     console.log(value2 +": " + typeof(value2));
+//     return idObj;
+// }
+// const idObj1 = {
+//     value1: "Hello",
+//     value2: "World"
+// };
+// const idObj2 = {
+//     value1: 2,
+//     value2: true
+// };
+// console.log(identity(idObj1));
+// console.log(identity(idObj2));
+class Identity {
+    constructor(initialVal) {
+        this.value = initialVal;
+    }
+    getValue() {
+        return this.value;
+    }
 }
-const idObj1 = {
-    value1: "Hello",
-    value2: "World"
-};
-const idObj2 = {
-    value1: 2,
-    value2: true
-};
-console.log(identity(idObj1));
-console.log(identity(idObj2));
+let myVal = new Identity(5);
+let result = myVal.getValue();
+console.log(result);
--- index.ts
+++ index.ts
@@ -1,24 +1,40 @@
-interface Identity <T, U> {
-    value1: T,
-    value2: U
+// interface Identity <T, U> {
+//     value1: T,
+//     value2: U
+// }
+
+// function identity<T,U>(idObj: Identity<T, U>): Identity<T,U> {
+//     let {value1, value2} = idObj;
+//     console.log(value1 +": " + typeof(value1));
+//     console.log(value2 +": " + typeof(value2));
+//     return idObj;
+// }
+
+// const idObj1 = {
+//     value1: "Hello",
+//     value2: "World"
+// };
+
+// const idObj2 = {
+//     value1: 2,
+//     value2: true
+// };
+
+// console.log(identity(idObj1));
+// console.log(identity(idObj2));
+
+class Identity<T> {
+    value: T;
+    
+    constructor(initialVal: T) {
+     this.value = initialVal;
+    }
+
+    getValue(): T {
+        return this.value;
+    }
 }
 
-function identity<T,U>(idObj: Identity<T, U>): Identity<T,U> {
-    let {value1, value2} = idObj;
-    console.log(value1 +": " + typeof(value1));
-    console.log(value2 +": " + typeof(value2));
-    return idObj;
-}
-
-const idObj1 = {
-    value1: "Hello",
-    value2: "World"
-};
-
-const idObj2 = {
-    value1: 2,
-    value2: true
-};
-
-console.log(identity(idObj1));
-console.log(identity(idObj2));
+let myVal = new Identity<number>(5);
+let result: number = myVal.getValue();
+console.log(result);
