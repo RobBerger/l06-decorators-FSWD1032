e393bd8fbb09d422b8f35d62b0df7e707947fef7
berger <robertberger.dev@gmail.com>
2023-01-26 10:53:45 -0500

    ran npm start

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 //     value1: T,
 //     value2: U
 // }
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 // function identity<T,U>(idObj: Identity<T, U>): Identity<T,U> {
 //     let {value1, value2} = idObj;
 //     console.log(value1 +": " + typeof(value1));
@@ -19,14 +25,46 @@
 // };
 // console.log(identity(idObj1));
 // console.log(identity(idObj2));
-class Identity {
-    constructor(initialVal) {
-        this.value = initialVal;
+// class Identity<T> {
+//     value: T;
+//     constructor(initialVal: T) {
+//      this.value = initialVal;
+//     }
+//     getValue(): T {
+//         return this.value;
+//     }
+// }
+// let myVal = new Identity<number>(5);
+// let result: number = myVal.getValue();
+// console.log(result);
+class Contact {
+    constructor(fname, lname) {
+        this.firstName = fname;
+        this.lastName = lname;
     }
-    getValue() {
-        return this.value;
+    getFullName() {
+        return `Full Name: ${this.firstName} ${this.lastName}`;
+    }
+    greetName(greeting) {
+        console.log(`${greeting} ${this.firstName}`);
     }
 }
-let myVal = new Identity(5);
-let result = myVal.getValue();
-console.log(result);
+__decorate([
+    wrapper
+], Contact.prototype, "getFullName", null);
+__decorate([
+    wrapper
+], Contact.prototype, "greetName", null);
+function wrapper(target, propertyKey, descriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args) {
+        console.log(`Calling ${propertyKey}() with`, args);
+        const result = originalMethod.apply(this, args);
+        console.log(`${propertyKey}() finished and returned ${result}`);
+        return result;
+    };
+    return descriptor;
+}
+let contact = new Contact('Bob', 'Bobertson');
+contact.getFullName();
+contact.greetName('Howdy');
