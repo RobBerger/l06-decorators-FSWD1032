3056fd8a80291393d873fe3eb9c5bed5f4644363
berger <robertberger.dev@gmail.com>
2023-01-26 09:44:55 -0500

    added example code

---
+++ index.ts
@@ -0,0 +1,12 @@
+const idObj1 = {
+    value1: "Hello",
+    value2: "World"
+};
+
+const idObj2 = {
+    value1: 2,
+    value2: true
+};
+
+console.log(identity(idObj1));
+console.log(identity(idObj2));
